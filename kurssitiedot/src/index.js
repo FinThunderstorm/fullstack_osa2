63fc9de15e8f4971ca9a2ab6808c684387df9e77
Tuomas Alanen <tuomas@paaryna.fi>
2019-06-16 22:03:21 +0300

    2.5
    kurssitiedot moduuliesitys

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -1,53 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-    return(
-        <>
-            <h1>{props.course}</h1>
-        </>
-    )
-}
-
-const Part = (props) => {
-    return(
-        <>
-            <p>{props.part} {props.exercises}</p>
-        </>
-    )
-}
-
-const Content = (props) => {
-    return (
-        <>
-            {props.parts.map(part => {
-                return <Part key={part.id} part={part.name} exercises={part.exercises}/>
-            })}
-        </>
-    )
-}
-
-const Total = (props) => {
-    // logiikka siis se, että annetaan nytille lähtöarvoksi nolla, johon se aina lisää lisayksen tehtavien määrän. (note to myself tulevaisuuteen)
-    const yhteensa = props.parts.reduce((nyt, lisays) => {
-        return (nyt + lisays.exercises)
-    },0)
-    return (
-        <>
-            <p>yhteensä {yhteensa} tehtävää</p>
-        </>
-    )
-}
-
-const Course = (props) => {
-    return (
-        <>
-            <Header course={props.course.name} />
-            <Content parts={props.course.parts} />
-            <Total parts={props.course.parts} />
-        </>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
@@ -93,11 +46,11 @@ const App = () => {
         }
     ]
   
-    const courseselemets = () => courses.map((course,i) => {
-        return (
-            <Course key={i} course={course}/>
-        )
-    })
+const courseselemets = () => courses.map((course,i) => {
+    return (
+        <Course key={i} course={course}/>
+    )
+})
     return (
         <div>
             {courseselemets()}
