d43425057214b7716bf923df3a616d736c062c71
Tuomas Alanen <tuomas@paaryna.fi>
2019-06-18 11:21:04 +0300

    2.9
    puhelinluettelo step4

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,6 +27,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -37,12 +39,32 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const rows = () => persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+
+
+  const rows = () => persons.filter(person => {
+    if(newFilter.length > 0){
+      const nameLowerCase = person.name.toLowerCase()
+      console.log(person.name," ",nameLowerCase)
+      const filter = newFilter.toLowerCase()
+      console.log(newFilter," ",filter)
+      return nameLowerCase.includes(filter)
+    } else{
+      return true
+    }
+   }).map(person => <div key={person.name}>{person.name} {person.number}</div>)
   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>filter shown with <input value={newFilter} onChange={handleFilterChange}/></div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handlePersonChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
